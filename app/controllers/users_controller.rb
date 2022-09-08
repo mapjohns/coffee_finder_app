ca241af615baa3ab241d898e8b4e8acc64527d62
Matthew <mattj20@gmail.com>
2022-09-07 21:10:07 -0700

    Added methods to controller and user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
 
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    private
+
+    def user_params(params)
+        params.require(:user).permit(:username, :first_name, :last_name, :password)
+    end
+
 end
