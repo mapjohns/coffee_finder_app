dda8c379175b5f27c000252fa10edcf970888ae6
Matthew <mattj20@gmail.com>
2022-09-09 12:26:01 -0700

    Updated validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
     validates :username, uniqueness: true
+    validates :password, :password_confirmation, :username, :first_name, :last_name, presence: true
     
 end
