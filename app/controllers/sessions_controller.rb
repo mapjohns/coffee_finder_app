bad012ef3e2b401a557d9e01e511f7e435e21a54
Matthew <mattj20@gmail.com>
2022-09-10 19:44:03 -0700

    Updated create to handle login validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,13 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(:username => params[:username])
+        if user && !!user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:notice] = "Please enter valid credentials"
+            render 'new'
+        end
     end
 end
