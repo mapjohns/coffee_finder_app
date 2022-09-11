bbc48962d9c420f57cb00bcb51d3c3b45b6f3391
Matthew <mattj20@gmail.com>
2022-09-10 19:09:49 -0700

    Added signin route for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+
   get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
 end
