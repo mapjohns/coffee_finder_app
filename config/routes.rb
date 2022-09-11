7a12f141431595ad8af99a3ca25200891afe81db
Matthew <mattj20@gmail.com>
2022-09-10 20:52:00 -0700

    Added logout route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
 end
