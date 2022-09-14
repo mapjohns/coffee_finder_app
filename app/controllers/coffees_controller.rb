2609bf36ecf74feee4e2e4f4be419e0e34e2632d
Matthew <mattj20@gmail.com>
2022-09-14 13:13:12 -0700

    Added methods and params method

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,3 +1,25 @@
 class CoffeesController < ApplicationController
     
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    private
+
+    def coffee_params
+        params.require(:coffee).permit(:drink_name, :description, :store_id)
+    end
 end
