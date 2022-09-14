20ad1f667b83644de730fb3a7c78571a55e4de09
Matthew <mattj20@gmail.com>
2022-09-13 21:32:06 -0700

    Create method updated to handle GitHub sign-in flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,8 @@ class SessionsController < ApplicationController
     end
 
     def create
-        binding.pry
-        user = User.find_by(:username => params[:username])
-        if user && !!user.authenticate(params[:password])
+        user = !!auth ? User.oauth(auth) : User.find_by(:username => params[:username])
+        if (user && !!user.authenticate(params[:password])) || (!!user.g_id == true if !!auth)
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
