43d333691b8736f258aa1afde58d045cee5d3843
Matthew <mattj20@gmail.com>
2022-09-14 13:41:17 -0700

    Filled out create method and added/updated Index method

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,10 +1,20 @@
 class CoffeesController < ApplicationController
     
+    def index
+        @coffees = Coffee.all
+    end
     def new
         @coffee = Coffee.new
     end
 
     def create
+        @coffee = Coffee.new(coffee_params)
+        if @coffee.valid?
+            @coffee.save
+            redirect_to 
+        else
+          render 'new'  
+        end
     end
 
     def show
