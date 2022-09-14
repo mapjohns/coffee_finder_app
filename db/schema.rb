cf7f958702d0ba90e139a8e69650835c578fc36b
Matthew <mattj20@gmail.com>
2022-09-13 21:44:44 -0700

    Coffees table added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_13_053506) do
+ActiveRecord::Schema.define(version: 2022_09_14_044041) do
+
+  create_table "coffees", force: :cascade do |t|
+    t.string "drink_name"
+    t.string "description"
+    t.integer "store_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
