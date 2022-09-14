d337cdfac2b036232d9f746aadf15bfb4d2f7e5e
Matthew <mattj20@gmail.com>
2022-09-14 13:09:38 -0700

    Added nested routes to users and added routes for coffees

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users do
+    resources :coffees, only: [:index, :show]
+  end
 
+  resources :coffees
+  
   get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
