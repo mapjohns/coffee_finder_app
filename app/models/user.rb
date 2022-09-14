3f76a41a4ae596465490268384e584d96f522dc8
Matthew <mattj20@gmail.com>
2022-09-13 21:07:31 -0700

    Created method to handle User creation from GitHub Login flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
     validates :username, uniqueness: true
     validates :password_confirmation, :username, :first_name, :last_name, presence: true
     
+    def self.oauth(auth)
+        user = User.find_or_create_by(g_id: auth['uid']) do |u|
+            u.first_name = auth['info']['name'].split[0]
+            u.last_name = auth["info"]["name"].split[1..-1].join(" ")
+            u.username = SecureRandom.base64(10)
+            u.password = SecureRandom.base64(50)
+            u.password_confirmation = u.password
+            u.g_id = auth['uid']
+        end
+    end
 end
