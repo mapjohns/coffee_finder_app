9f340aa8eae6061cfbacbcf3642914c4b90508d8
Matthew <mattj20@gmail.com>
2022-09-15 20:01:11 -0700

    Added belongs_to relationship with user and coffee

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
     validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5}
     validates :content, :rating, presence: true
+
+    belongs_to :user
+    belongs_to :coffee
+    
 end
