f3b9d41a0a0ea5062e1221359b8a18bae2631c67
Matthew <mattj20@gmail.com>
2022-09-16 17:33:39 -0700

    Added methods for pulling current user and checking if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    def logged_in?
+        if current_user == nil
+            redirect_to '/signin'
+        end
+    end
+
+    def current_user
+        user = User.find_by(id: session[:user_id])
+        user.nil? ? nil : user
+    end
 end
