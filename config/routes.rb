a7839a5f08884f4952adff8f361b1d7d8bdbd3a0
Matthew <mattj20@gmail.com>
2022-09-16 17:43:21 -0700

    Added routes for Reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :coffees, only: [:index, :show]
+    resources :reviews, only: [:index, :show]
   end
 
   resources :coffees
+  resources :reviews
   
   get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
