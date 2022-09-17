5a99cd304eba404afcdfaaae0e8b49cfce135cf1
Matthew <mattj20@gmail.com>
2022-09-17 16:23:06 -0700

    Created helper with conditional logic to be used for the top bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def top_bar
+        if current_user
+            link_to "Log out", "/logout"
+        else
+            link_to "Log-in", "/signin"
+        end
+    end
 end
