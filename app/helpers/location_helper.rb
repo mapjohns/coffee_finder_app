b368e0f709be741160729d9be3fa05028d0ba3b9
Matthew <mattj20@gmail.com>
2022-09-23 13:18:21 -0700

    Updated helper to account for stores without locations

--- app/helpers/location_helper.rb
+++ app/helpers/location_helper.rb
@@ -1,6 +1,6 @@
 module LocationHelper
     def store_name
-        if params[:store_id]
+        if params[:store_id] && @locations.present? == true
             "#{@locations.first.store.name} Locations:"
         else
             "Locations:"
