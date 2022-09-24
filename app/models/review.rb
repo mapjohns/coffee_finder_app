c648f3af1b2f6d54544354ef0e166eb2c7d1f4c9
Matthew <mattj20@gmail.com>
2022-09-24 16:08:43 -0700

    Added validations for users who submit multiple reviews to the same coffee

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,14 @@
 class Review < ActiveRecord::Base
     validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5}
     validates :content, :rating, presence: true
+    validate :multiple_reviews, on: :create
+    validates :user_id, uniqueness: { message: "You have already reviewed this coffee." }, if: :multiple_reviews 
 
     belongs_to :user
     belongs_to :coffee
     
+
+    def multiple_reviews
+        self.coffee.users.include?(self.user)
+    end
 end
