4e028a1397a1358fa192eb11e2fce570b9593aec
Matthew <mattj20@gmail.com>
2022-09-24 19:09:16 -0700

    Removed redundant validation and updated user_id to trigger on create

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,7 @@
 class Review < ActiveRecord::Base
     validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5}
     validates :content, :rating, presence: true
-    validate :multiple_reviews, on: :create
-    validates :user_id, uniqueness: { message: "You have already reviewed this coffee." }, if: :multiple_reviews 
+    validates :user_id, uniqueness: { message: "You have already reviewed this coffee." }, if: :multiple_reviews, on: :create
 
     belongs_to :user
     belongs_to :coffee
