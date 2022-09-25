c97fd412ca905b25571ec95687ef85da100da187
Matthew <mattj20@gmail.com>
2022-09-25 06:19:47 -0700

    Updated user_id error message to flow better with the attribute

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
     validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5}
     validates :content, :rating, presence: true
-    validates :user_id, uniqueness: { message: "You have already reviewed this coffee." }, if: :multiple_reviews, on: :create
+    validates :user_id, uniqueness: { message: "has already reviewed this coffee." }, if: :multiple_reviews, on: :create
 
     belongs_to :user
     belongs_to :coffee
