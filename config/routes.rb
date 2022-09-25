68d1cb8e9d8b219cbddaf73c43ac88a8b28901ff
Matthew <mattj20@gmail.com>
2022-09-24 18:35:32 -0700

    Added edit/update to nested user route for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :coffees, only: [:index, :show]
-    resources :reviews, only: [:index, :show]
+    resources :reviews, only: [:index, :show, :edit, :update]
   end
 
   resources :stores do
