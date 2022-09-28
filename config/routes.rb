b38f5ce12968f9d613a213780fb6ae6137806973
Matthew <mattj20@gmail.com>
2022-09-28 07:30:03 -0700

    Added route for highest_rated coffee

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/coffees/most_recent', to: 'coffees#most_recent'
+  get '/coffees/highest_rated', to: 'coffees#highest_rated'
   get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
