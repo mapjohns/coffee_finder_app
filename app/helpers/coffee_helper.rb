3b855cd090f99df745787a8696b5376d6cecb281
Matthew <mattj20@gmail.com>
2022-09-30 16:22:51 -0700

    Updated to use find_by

--- app/helpers/coffee_helper.rb
+++ app/helpers/coffee_helper.rb
@@ -1,6 +1,6 @@
 module CoffeeHelper
 
     def avg_rating
-        Coffee.find(params[:id]).reviews.average(:rating).to_f.round(2)
+        Coffee.find_by(id: params[:id]).reviews.average(:rating).to_f.round(2)
     end
 end
--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,10 +1,10 @@
 module StoreHelper
 
     def num_locations
-        Store.find(params[:id]).locations.count
+        Store.find_by(id: params[:id]).locations.count
     end
 
     def num_coffee
-        Store.find(params[:id]).coffees.count
+        Store.find(id: params[:id]).coffees.count
     end
 end
