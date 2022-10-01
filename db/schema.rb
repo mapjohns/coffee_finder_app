5eaff4c6c129c9ed4eb9e47abca976f8dc7ca92e
Matthew <mattj20@gmail.com>
2022-09-30 17:00:02 -0700

    Re-made migration file and ran migration for adding admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_30_234442) do
+ActiveRecord::Schema.define(version: 2022_09_30_235829) do
 
   create_table "coffees", force: :cascade do |t|
     t.string "drink_name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2022_09_30_234442) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "g_id"
+    t.boolean "admin", default: false
   end
 
 end
