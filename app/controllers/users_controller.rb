ddea2d41f0a3314a52d99c5dc07947e97af6039e
Matthew <mattj20@gmail.com>
2022-09-30 17:03:28 -0700

    Added Admin checkbox and added to params for user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     end
 
     def create
+        binding.pry
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
@@ -30,7 +31,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :first_name, :last_name, :password,:password_confirmation)
+        params.require(:user).permit(:username, :first_name, :last_name, :password,:password_confirmation, :admin)
     end
 
 end
