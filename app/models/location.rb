bcc25f8d7dfc6c57fac91f61548e0e76a3ead185
Matthew <mattj20@gmail.com>
2022-10-02 17:14:20 -0700

    Updated store validation to account for updating a location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ActiveRecord::Base
     validates :street, :city, :state, :zip_code, :store_id, presence: true
+    validates :store_id, presence: true, on: :create
     belongs_to :store
 end
