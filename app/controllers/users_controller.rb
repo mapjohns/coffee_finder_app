c04be3c68bf26a986cc0df3e89e72bd30822e040
Matthew <mattj20@gmail.com>
2022-10-02 20:08:23 -0700

    Fixed, so that create does not get redirected

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :logged_in?, only: [:new]
+    skip_before_action :logged_in?, only: [:new, :create]
 
     def new
         @user = User.new
