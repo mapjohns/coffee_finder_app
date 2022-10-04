46a23102af7a225147c4b28a7578ad7a17766ebb
Matthew <mattj20@gmail.com>
2022-10-03 21:06:53 -0700

    Created helper to implement display logic in coffee index

--- app/helpers/coffee_helper.rb
+++ app/helpers/coffee_helper.rb
@@ -3,4 +3,12 @@ module CoffeeHelper
     def avg_rating
         Coffee.find_by(id: params[:id]).reviews.average(:rating).to_f.round(2)
     end
+
+    def coffee_index_wording
+        if params[:store_id]
+            "Below are all the coffees available so far for #{@coffees.first.store.name}!"
+        else
+            "Below are all the coffees available so far!"
+        end
+    end
 end
