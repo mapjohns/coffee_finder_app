038bb98602ce9720036e8ec2ce1fcc482178581b
Matthew <mattj20@gmail.com>
2022-10-04 13:38:52 -0700

    Added validation for numericality of zip code

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ActiveRecord::Base
     validates :street, :city, :state, :zip_code, :store_id, presence: true
     validates :store_id, presence: true, on: :create
+    validates_numericality_of :zip_code
     belongs_to :store
 end
